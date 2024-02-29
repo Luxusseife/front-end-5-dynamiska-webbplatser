aa8221091d8e6adbb1003f034763417cad86bf24
Jenny Lind <jeli2308@student.miun.se>
2024-02-29 21:04:23 +0100

    Lagt till en p i sök-containern i map.html för felmeddelanden, stylat hover för knapp, lagt en händelselyssnare på knappen som kör funktionen getInput som i sin tur anropar getData (fetch). showMap anropas som ställer om karta och markör till vald plats.

--- src/JS/map.js
+++ src/JS/map.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Deklarerar variabler.
+let searchButtonEl = document.getElementById("search-button");
+
 // Initierar kartan och ställer in vy efter valda koordinater (Bjärnum, min hemort).
 let map = L.map('map').setView([56.294167, 13.711111], 12);
 
@@ -8,10 +11,45 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     // Ställer in start-zoom.
     maxZoom: 17,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    }).addTo(map);
 
 // Lägger till en markör på kartan utifrån valda koordinater.
 let marker = L.marker([56.294167, 13.711111]).addTo(map);
 
+// Händelselyssnare.
+searchButtonEl.addEventListener("click", getInput);
 
+// Funktion som hämtar input från sökfältet och anropar fetch-funktionen.
+function getInput() {
+    const searchInput = document.getElementById("search-input").value;
+    // Skickar med inputen. 
+    getData(searchInput);
+};
 
+// Hämtar data från OpenStreetMap med värdet från sökfältet.
+async function getData(searchInput) {
+    try {
+        const response = await fetch("https://nominatim.openstreetmap.org/search?addressdetails=1&q=" + searchInput + "&format=jsonv2&limit=1");
+        let data = await response.json();
+
+       // Anropar kartvisningsfunktion.
+       showMap(data);
+
+        // Felmeddelande
+        } catch (error) {
+            console.error("Kunde inte fetcha, följande felmeddelande skapades:", error);
+          }
+}
+
+// Kartvisnings-funktion.
+function showMap(data) {
+    // Lagrar latitud och longitud i variabler. Hittar lat/long i index 0.
+    let latitude = data[0].lat;
+    let longitude = data[0].lon;
+
+    // Ställer om markören utifrån sökresultatet.
+    marker.setLatLng([latitude, longitude]);
+
+    // Ställer om kartan utifrån sökresultatet.
+    map.setView([latitude, longitude], 12);
+}
